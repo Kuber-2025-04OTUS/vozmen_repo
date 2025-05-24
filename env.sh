130780c6e222ae2c71e72e759442bfb91a95e48e
Vozdvizhensky Ilya <i.vozdvizhensky@netnoir.ru>
2025-05-24 23:36:23 +0300

    change ingress-nginx

--- env.sh
+++ env.sh
@@ -3,6 +3,6 @@ TOKEN=$(kubectl get secret homework-secret -n homework -o jsonpath='{.data.token
 URL=$(kubectl config view -o jsonpath='{.clusters[0].cluster.server}')
 CA=$(kubectl get secret homework-secret -n homework -o jsonpath='{.data.ca\.crt}')
 
-sed -i 's|certificate-authority-data: .*|certificate-authority-data: '$CA'|g' kubernetes-security/kubeconfig.yaml
-sed -i 's|server: .*|server: '$URL'|g' kubernetes-security/kubeconfig.yaml
-sed -i 's|token: .*|token: '$TOKEN'|g' kubernetes-security/kubeconfig.yaml
+sed -i 's|certificate-authority-data: .*|certificate-authority-data: '$CA'|g' templates/kubernetes-security/kubeconfig.yaml
+sed -i 's|server: .*|server: '$URL'|g' templates/kubernetes-security/kubeconfig.yaml
+sed -i 's|token: .*|token: '$TOKEN'|g' templates/kubernetes-security/kubeconfig.yaml
